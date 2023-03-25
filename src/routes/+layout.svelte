890ad3bedd53ec3b066de00cf5477d60b7152bc2
Andrew Kraevskii <andrew.kraevskii@gmail.com>
2023-03-25 06:16:04 +0300

    something

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "../app.css";
+</script>
+
+<header class="bg-blue-200 flex gap-2">
+    <h1 class=""><a href="/">Itmohub</a></h1>
+    <nav class="bg-blue-200">
+        <ul>
+            <a href="/about">about</a>
+        </ul>
+    </nav>
+</header>
+
+<slot />
